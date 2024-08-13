f669f4577a4638161b5b987f468a09367fc88ba6
Ibrahim Algadi <ibrahimalgadi.email@gmail.com>
2024-08-13 23:36:32 +0400

    Adding Custom Middleware

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,9 @@ import {ValidationPipe} from "@nestjs/common";
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
+
+  // register global middlewares
+  // app.use(cors(). helmet(), multer());
   await app.listen(3000);
 }
 bootstrap();
